4a2558284c09ae67c06a0973c52e92a5556e5d12
Daniel Ebron <dallas.ebron@gmail.com>
2021-12-10 11:10:01 -0500

    Preparations Section excersizes

---
+++ Preparations/preparations_exercises/foo.js
@@ -0,0 +1,73 @@
+let foo = "bar";
+console.log(foo);
+foo;
+
+// 5.
+// Identify the Constructors for each of the following methods and classify each method as either a "Static" or a "Prototype" method:
+
+// substring         Prototype/Instance
+// create            Static
+// fromCharCode      Static
+// slice             Prototype/Instance
+// toString          Prototype/Instance
+// // It might be that more than one constructor provides a method with a given name. Make sure you list them all in your answer. You can limit your search for methods to the String, Object, Array, and Number constructors.
+
+
+// 6.
+// Which of the following names satisfy the style guidelines for variable names? For the purposes of this question, constants are not variables.
+
+// Name
+
+// index                    THIS ONE!!!
+// CatName
+// snake_case   THIS ONE    ANSWER: NOT THIS ONE
+// lazyDog      THIS ONE    ANSWER: CORRECT
+// quick_Fox    THIS ONE?   ANSWER: NOT THIS ONE
+// 1stCharacter
+// operand2     THIS ONE?   ANSWER: CORRECT
+// BIG_NUMBER   THIS ONE    ANSWER: NOT THIS ONE
+
+
+// 7.
+// Which of the following names satisfy the style guidelines for function names?
+
+// Name
+
+// index                    THIS ONE!!!
+// CatName      THIS ONE    ANSWER: CORRECT       NOTE:constructor functions can use PascalCase names like CatName
+// snake_case
+// lazyDog      THIS ONE    ANSWER: CORRECT 
+// quick_Fox
+// 1stCharacter
+// operand2                 THIS ONE
+// BIG_NUMBER   THIS ONE    THIS ONE    ANSWER: NOT THIS ONE
+
+
+// 8.
+// Which of the following names satisfy the style guidelines for constants used to represent MAGIC NUMBERS?  (What is a magic number)
+
+// Name
+
+// index        THIS ONE    ANSWER: NOT THIS ONE
+// CatName
+// snake_case
+// lazyDog      THIS ONE    ANSWER: NOT THIS ONE
+// quick_Fox
+// 1stCharacter
+// operand2     THIS ONE    ANSWER: NOT THIS ONE
+// BIG_NUMBER   THIS ONE!!!!!
+
+
+// 9.
+// Which of the following names don't satisfy the style guidelines for naming variables, functions, or constants?
+
+// Name
+
+// index
+// CatName
+// snake_case    THIS ONE
+// lazyDog
+// quick_Fox     THIS ONE
+// 1stCharacter  THIS ONE
+// operand2
+// BIG_NUMBER    
