ec4500f6b58c731c320ef39f94441a7eef9af7e8
Daniel Ebron <dallas.ebron@gmail.com>
2021-12-16 17:34:47 -0500

    arrays incomplete.  need to revisit and restudy

---
+++ IntroToProgramming/Arrays/excerize2.js
@@ -0,0 +1,28 @@
+// let myArray = [1, 3, 6, 11, 4, 2,
+//   4, 9, 17, 16, 0];
+
+
+// myArray.forEach(function(value) {
+//   if (value % 2 === 0) {
+//     console.log(value);
+//   }
+// })
+
+
+
+
+
+// myArray.forEach(function(evenNumbers){
+//   evenNumbers.forEach(function(individualNumbers){
+//     if ( individualNumbers % 2 === 0) {
+//       console.log(individualNumbers);
+//     };
+//   })
+// })
+
+// let myArray = [
+//   [1, 3, 6, 11],
+//   [4, 2, 4],
+//   [9, 17, 16, 0],
+// ];
+
